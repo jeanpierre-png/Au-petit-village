470ba0923500d853e9a4246b9f2a053d46bec45f
NOZA Jean-Pierre <nozajean-pierre@MacBookAir.mshome.net>
2024-11-03 15:49:37 -0300

    Ajout d'éléments pour la page home

--- src/app/Pages/produit/produit.component.ts
+++ src/app/Pages/produit/produit.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, ProductsService } from '../../service/products.service';
 
 @Component({
   selector: 'app-produit',
   templateUrl: './produit.component.html',
   styleUrl: './produit.component.css'
 })
-export class ProduitComponent {
+
+export class ProduitComponent implements OnInit {
+
+  product: Product | undefined;
+
+  route: ActivatedRoute = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  ngOnInit() {
+    const productId = Number(this.route.snapshot.params['id']);
+
+    this.product = this.productsService.getProductById(productId);
+  }
 
 }
